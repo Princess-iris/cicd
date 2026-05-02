5d4161c93dec9ecc664ea50b3be4720f60f20c42
Gayos, Princess Iris A. <23-75248@g.batstate-u.edu.ph>
2026-05-02 07:17:55 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>CI/CD Pipeline Test</title>
-</head>
-<body style="background-color:brown">
-    <h1><?php echo "Hello CI/CD World"; ?></h1>
-    <p>Deployed by Jenkins at <?php echo date('Y-m-d H:i:s'); ?></p>
+<body style="background-color:pink">
+    <h1><?php
+        echo "hello world"; 
+    ?></h1>
 </body>
-</html>
